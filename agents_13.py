db9f80bd271ca182768ac3dfba2aa4b1dc06a567
Pedro Carrega <fc49480@alunos.fc.ul.pt>
2019-11-25 11:30:09 +0000

    creating eval_fn functions

---
+++ agents_13.py
@@ -0,0 +1,27 @@
+from pacman import Directions
+
+
+import game
+
+
+
+from util import manhattanDistance
+from game import Directions
+import random, util
+
+from game import Agent
+
+from collections import namedtuple
+import random
+import multiAgents
+
+from utils import argmax
+
+def pac_13(gState, player):
+
+
+
+
+
+def fant_13(gState, player):
+
