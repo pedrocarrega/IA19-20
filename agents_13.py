e142a2b1a7fa7185828e5b3fa97cfe49b2399d29
Pedro Carrega <fc49480@alunos.fc.ul.pt>
2019-12-02 10:51:39 +0000

    trying to make the function work

--- agents_13.py
+++ agents_13.py
@@ -5,7 +5,7 @@ import game
 
 
 
-from util import manhattanDistance
+from multiAgents import manhattanDistance
 from game import Directions
 import random, util
 
@@ -18,10 +18,17 @@ import multiAgents
 from utils import argmax
 
 def pac_13(gState, player):
+    foodList = gState.board.getFood().asList()
+    minDistance = 0
+    # Compute distance to the nearest food
+    if len(foodList) > 0: # This should always be True,  but better safe than sorry
+        myPos = gState.board.getPacmanPosition()
+        
+        minDistance = min([manhatanDist(myPos, food) for food in foodList])
+    return gState.board.getScore() * 100 - minDistance
 
 
 
 
-
-def fant_13(gState, player):
+#def fant_13(gState, player):
 
