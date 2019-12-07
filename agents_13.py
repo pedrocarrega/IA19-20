61bff965477988ec61014538e475b77986b1e476
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2019-12-07 04:47:39 +0000

    tried to add the extra
    still no real progress tho

--- agents_13.py
+++ agents_13.py
@@ -17,6 +17,23 @@ import multiAgents
 
 from utils import argmax
 
+def extraP_13(gState,extra):
+    """ vai memorizar onde se encontra o fantasma e as super pastilhas, pq no scooore por vezes
+        o efeito das superpastlhas n eh aproveitado -> arranjar maneira do gajo so apanhar a pastilha
+        quando realmente de para comer o fantasma -> mas optando no entanto por nao comer o fantasma
+        caso esteja a x casas da posicao onde o fantasma respawns (acho que no scooore isto ja acontece)
+    """
+    if extra == {}:
+        n_extra ={'super':[gState.getCapsules()],'Ghosts':[gState.getGhostPosition(1)],}
+        return n_extra
+    else:
+        n_extra=extra.copy()
+        n_extra['super'] =[gState.getCapsules()] + n_extra['super'] #?
+        n_extra['Ghosts']=[gState.getGhostPosition(1)]+n_extra['Ghosts']
+    return n_extra
+#outra cena a alterar se possivel seria diminuir as vezes que o pacman fica indeciso,  -> ver recordedgamepac_404 
+#alguma ideia porque isto acontece?
+
 def pac_13(gState, player):
     foodList = gState.board.getFood().asList()
     minDistance = 0
